c9ce70d46bbe03545b936dbbe2240cb601f1c0ff
Saurabh Sharma <saurabh.sharma983@gmail.com>
2021-03-23 19:09:22 +0530

    Resizable view

---
+++ app/build/generated/source/buildConfig/debug/me/waveproject/widget/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.waveproject.widget;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "me.waveproject.widget";
+  public static final String BUILD_TYPE = "debug";
+}
